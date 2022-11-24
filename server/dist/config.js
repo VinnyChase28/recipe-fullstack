655d3ac028e7f0373afd22aa499cac6e6cff26bd
VinnyChase28 <vincentgauthier068@gmail.com>
2022-11-24 13:32:39 -0800

    add express and prima

--- server/dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = exports.SERVER_PORT = exports.CLIENT_URL = void 0;
-const client_1 = require("@prisma/client");
-exports.CLIENT_URL = process.env.CLIENT_URL;
-exports.SERVER_PORT = process.env.SERVER_PORT;
-exports.prisma = new client_1.PrismaClient();
-//# sourceMappingURL=config.js.map
